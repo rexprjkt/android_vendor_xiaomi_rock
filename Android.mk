85eb05247a529f3aec3a21f45368d77ece882a08
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-05-07 06:16:39 +0000

    rock: Initial vendor tree
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),rock)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,21d3fc2197ac9510f50306e61602d26a2576aa81)
+$(call add-radio-file-sha1-checked,radio/gz.img,2571d677e5b2ec0f4fdaf66f365942c9cada82d2)
+$(call add-radio-file-sha1-checked,radio/lk.img,1a468c20305cc1a4ef0345f520046bc95d13c3a6)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,56983eeb0c9b8924d5e8a5fd740dab3fd815a530)
+$(call add-radio-file-sha1-checked,radio/md1img.img,c9d7c7d05d85e5c8e6bbce731bd39cdb3cdd8093)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,e2bb3969cdcef3cd1a85274da3c5b9b05ff0d3b4)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,7b98750858165a58ebfa65bfe7c01112c5198efd)
+$(call add-radio-file-sha1-checked,radio/scp.img,d0a740cf09428617769c1f34b108040ee7588b8e)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,c988de163d89362cf30c730aa63611c42fc05fc6)
+$(call add-radio-file-sha1-checked,radio/sspm.img,75045b98a0275ed248e057b78fc3f2bd597bb177)
+$(call add-radio-file-sha1-checked,radio/tee.img,5f3e13a2f0e3eb42cc7280f004ab04e3f6710b4c)
+
+endif
